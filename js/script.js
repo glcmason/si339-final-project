d77167cf4853da99ecf5d0c15c02c5624e9f6b24
Grace <113309727+glcmason@users.noreply.github.com>
2024-12-04 02:32:07 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function uxLink() {
+    window.location.href = 'ux.html'; // URL of the target page
+}
+
+function artworkLink() {
+    window.location.href = 'artwork.html'; // URL of the target page
+}
+
+function photoLink() {
+    window.location.href = 'photography.html'; // URL of the target page
+}
